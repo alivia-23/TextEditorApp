c7f20c43495f86b9bd52f39df2935a1ba93d6f03
alivia-23 <guin.al@husky.neu.edu>
2023-12-08 13:47:40 -0800

    implement getNumSentences, getNumWords,getNumSyllables function

--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -34,9 +34,8 @@ public class BasicDocument extends Document
 	@Override
 	public int getNumWords()
 	{
-		//TODO: Implement this method in week 2 according to the comments above.  
-		// See the Module 2 support videos if you need help.
-	    return 0;
+		List<String> words = getTokens("[a-zA-Z]+");
+	    return words.size();
 	}
 	
 	/**
@@ -54,9 +53,8 @@ public class BasicDocument extends Document
 	@Override
 	public int getNumSentences()
 	{
-	    //TODO: Implement this method.  See the Module 2 support videos 
-        // if you need help.
-        return 0;
+		List<String> sentences = getTokens("[^.!?]+");
+        return sentences.size();
 	}
 	
 	/**
@@ -76,12 +74,29 @@ public class BasicDocument extends Document
 	@Override
 	public int getNumSyllables()
 	{
-	    //TODO: Implement this method in week 2.  See the Module 2 support videos 
-        // if you need help.  And note that there is no need to use a regular
-		// expression for the syllable counting.  We recommend you implement 
-		// the helper function countSyllables in Document.java using a loop, 
-		// and then call it here on each word.
-        return 0;
+		// use regexs to calculate the number of syllables and the other
+		// that finds words using a loop.  The regex solution is commented 
+		// out here at the top.
+
+		/* solution using regex's.  Uncoment here to run it*/
+		/*
+		List<String> tokens = getTokens("[aeiouyAEIOUY]+");
+		List<String> loneEs = getTokens("[^aeiouyAEIOUY]+[eE]\\b");
+		List<String> singleEs = getTokens("\\b[^aeiouyAEIOUY]*[eE]\\b");
+		
+		
+		return tokens.size() - (loneEs.size() - singleEs.size());
+		*/
+		
+		// solution that does NOT use regexs to find syllables 
+		List<String> tokens = getTokens("[a-zA-Z]+");
+		int totalSyllables = 0;
+		for (String word : tokens)
+		{
+			totalSyllables += countSyllables(word);
+		}
+		return totalSyllables;
+        
 	}
 	
 	
